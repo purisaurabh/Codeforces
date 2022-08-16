e27ca49d5f99beabf508a7e9eea58e8086721664
Immanuel <purisaurabh2885@gmail.com>
2022-08-16 22:19:09 +0530

    Solve

---
+++ Codeforces Maths Problem/18_candies_and_two_sister.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() 
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+        int n;
+        cin >> n;
+        if(n % 2 == 0) 
+            n -= 1;
+       cout << n/2 << endl;
+   }
+
+  return 0;
+}
