4ac259c4aa640e5477ed101ddf8cd8964db73db4
Immanuel <purisaurabh2885@gmail.com>
2022-08-23 19:55:38 +0530

    Solve

---
+++ Codeforces Maths Problem/25_dice_tower.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long int t;
+    cin >> t;
+    while (t--)
+    {
+        long long int x;
+        cin >> x;
+        if (x > 14 && x % 14 >= 1 && x % 14 <= 6)
+        {
+            cout << "YES" << endl;
+        }
+
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
